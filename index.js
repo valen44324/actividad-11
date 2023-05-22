47d6adc480372c726d367bb88001258a33ba0050
valen44324 <130708753+valen44324@users.noreply.github.com>
2023-05-21 21:04:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var nombre = "valentina"
+var apellido= "valdez"
+console.log(nombre + apellido );
+
+var numero1 = 10
+var numero2 = 5
+var suma = numero1 + numero2
+
+console.log(suma);
+var numero3 = suma + 10
+
+console.log(numero3);
+
